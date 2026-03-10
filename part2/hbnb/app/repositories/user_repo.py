313899de74844b5dca71be642c60866fde1ead73
soumaya <soumayabraziii@gmail.com>
2026-03-10 13:02:31 +0000

    fix: remove duplicate app project structure

--- part2/hbnb/app/repositories/user_repo.py
+++ part2/hbnb/app/repositories/user_repo.py
@@ -7,13 +7,13 @@ class UserRepository:
 
     def add(self, user: User):
         if not hasattr(user, "id") or user.id is None:
-            user.id = self.next_id
-            self.next_id += 1
+            import uuid
+            user.id = str(uuid.uuid4())
         self.users[user.id] = user
         return user
 
-    def get(self, user_id: int):
-        return self.users.get(int(user_id))
+    def get(self, user_id):
+        return self.users.get(str(user_id))
 
     def get_by_attribute(self, attr_name: str, value):
         for u in self.users.values():
@@ -24,7 +24,7 @@ class UserRepository:
     def get_all(self):
         return list(self.users.values())
 
-    def update(self, user_id: int, data: dict):
+    def update(self, user_id, data: dict):
         user = self.get(user_id)
         if not user:
             return None

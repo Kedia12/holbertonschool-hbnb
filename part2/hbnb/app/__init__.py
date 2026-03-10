78a10ed06c80c78057dd9ac19c2e90f59e908b46
soumaya <soumayabraziii@gmail.com>
2026-03-10 17:06:24 +0000

    fix: add namespace review

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -5,6 +5,7 @@ from flask_restx import Api
 from .api.v1.places import ns as places_ns
 from .api.v1.users import ns as users_ns
 from .api.v1.amenities import ns as amenities_ns
+from .api.v1.reviews import ns as reviews_ns
 
 def create_app():
     app = Flask(__name__)
@@ -13,5 +14,6 @@ def create_app():
     api.add_namespace(users_ns)
     api.add_namespace(places_ns)
     api.add_namespace(amenities_ns)
+    api.add_namespace(reviews_ns)
     
     return app
